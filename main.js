7aef59592e1bae9894b08fe64c2757a649c220f5
iwinter74 <isabelle.winternazlisoz@googlemail.com>
2020-07-15 17:00:14 +0200

    v.0.1.2

--- main.js
+++ main.js
@@ -16,3 +16,46 @@ function weather() {
     }    
     console.log(quality)
 }
+
+// function weather(quality) {
+//     if (quality <= 10 && quality >= 8) {
+//         console.log('super')
+//     } else if (quality < 8 && quality >= 6) {
+//         console.log('gut')
+//     } else if (quality < 6 && quality >= 3) {
+//         console.log('okay')
+//     } else if (quality < 3 && quality >= 0) {
+//         console.log('Schlecht')
+//     } else {
+//         console.log('only quality between 0 and 10')
+//     }
+// }
+// test
+// weather(9)
+// weather(7)
+// weather(5)
+// weather(2)
+// weather(-1)
+// weather(11)
+// function checkWeather() {
+//     console.log("It works")
+//     console.log(document.getElementById("qualityInput").value)
+//     weather(document.getElementById("qualityInput").value)
+// }
+//oder
+// function checkWeather() {
+//     console.log("It works")
+//     console.log(document.getElementById("qualityInput").value)
+//     let quality = document.getElementById("qualityInput").value
+//     if (quality <= 10 && quality >= 8) {
+//         console.log('super')
+//     } else if (quality < 8 && quality >= 6) {
+//         console.log('gut')
+//     } else if (quality < 6 && quality >= 3) {
+//         console.log('okay')
+//     } else if (quality < 3 && quality >= 0) {
+//         console.log('Schlecht')
+//     } else {
+//         console.log('only quality between 0 and 10')
+//     }
+// }
